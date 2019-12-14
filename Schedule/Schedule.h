04dbd3244c96269a90625094e2f18c3899782d74
Chris Zelazo <chris.zelazo@gmail.com>
2019-12-14 13:20:44 -0800

    Initial Commit

---
+++ Schedule/Schedule.h
@@ -0,0 +1,19 @@
+//
+//  Schedule.h
+//  Schedule
+//
+//  Created by Chris Zelazo on 12/14/19.
+//  Copyright © 2019 Chris Zelazo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Schedule.
+FOUNDATION_EXPORT double ScheduleVersionNumber;
+
+//! Project version string for Schedule.
+FOUNDATION_EXPORT const unsigned char ScheduleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Schedule/PublicHeader.h>
+
+
